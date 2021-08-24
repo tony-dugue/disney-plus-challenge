cb4dc4563d298c2ce0c60edbf43e0d23cf5e794f
tony dugué <duguetony@gmail.com>
2021-08-24 06:12:54 +0200

    add Page Detail (Movie description) + router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Detail from "./components/Detail";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+        <Router>
+            <Header />
+
+            <Switch>
+                <Route path="/detail"><Detail /></Route>
+                <Route path="/"><Home /></Route>
+            </Switch>
+
+        </Router>
     </div>
   );
 }
 
 export default App;
+
